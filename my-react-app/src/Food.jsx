b7c9cfbed18ed19f65b3fd1468bd047d9f506fa8
Chandirasegaran <chandirasegaransegar@gmail.com>
2024-02-24 20:40:23 +0530

    Intro To React

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,14 @@
+
+function Food(){
+    const food1="Idly";
+    const food2="Dosa";
+    return(
+        <ul>
+            <li>Pongal</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+
+export default Food
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Segar</p>
+        </footer>
+    );
+}
+
+export default Footer
