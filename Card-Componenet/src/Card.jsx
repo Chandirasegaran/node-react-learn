c280c3fe8697e602df07c62324594b1ef2e43a73
Chandirasegaran <chandirasegaransegar@gmail.com>
2024-03-04 13:38:34 +0530

    .

---
+++ Card-Componenet/src/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="profile picture"/>
+            <h2>Chandirasegaran</h2>
+            <p>Student at Pondicherry University</p>
+        </div>
+    );
+}
+export default Card
